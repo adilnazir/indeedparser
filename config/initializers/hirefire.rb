b9e6a85443cd69546a38410da9a67244f928fc2d
Adil Nazir <adil.nazir@tkxel.com>
2013-09-13 17:15:02 +0500

    bug fix hirefire

--- config/initializers/hirefire.rb
+++ config/initializers/hirefire.rb
@@ -1,10 +1,6 @@
 
 
 HireFire::Resource.configure do |config|
-  config.dyno(:resque_worker) do
-    HireFire::Macro::Resque.queue
-  end
-
   config.dyno(:dj_worker) do
     HireFire::Macro::Delayed::Job.queue
   end
